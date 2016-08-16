17f4a6ad2c881787fedd3ced4be6aa52e9740dd5
Keith Bentrup <keith@keithbentrup.com>
2016-08-16 18:24:11 -0400

    script to remove composer replace keys in CE from EE

---
+++ remove-ce-replacements-from-ee.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# stop on errors
+set -e
+# turn on debugging
+set -x
+
+SCRIPTS_DIR=$( cd $(dirname $0) ; pwd -P )
+
+# get array of keys from "replace" key of composer.json
+REPLACE_KEYS_FROM_M2CE=$(curl -s https://raw.githubusercontent.com/magento/magento2/develop/composer.json | jq -c '.replace | keys')
+
+# remove those keys from EE composer.json
+cat "${SCRIPTS_DIR}/../../magento/magento2ee/composer.json" | jq --indent 4 "del(.replace$REPLACE_KEYS_FROM_M2CE)" | tee "${SCRIPTS_DIR}/../../composer.json"
