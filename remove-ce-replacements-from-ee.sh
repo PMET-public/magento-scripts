a2f2c50b2242d84d53293d8856b7c35d50364927
Keith Bentrup <keith@keithbentrup.com>
2016-08-17 17:06:38 -0400

    update path of composer.json dest

--- remove-ce-replacements-from-ee.sh
+++ remove-ce-replacements-from-ee.sh
@@ -11,4 +11,4 @@ SCRIPTS_DIR=$( cd $(dirname $0) ; pwd -P )
 REPLACE_KEYS_FROM_M2CE=$(curl -s https://raw.githubusercontent.com/magento/magento2/develop/composer.json | jq -c '.replace | keys')
 
 # remove those keys from EE composer.json
-cat "${SCRIPTS_DIR}/../../magento/magento2ee/composer.json" | jq --indent 4 "del(.replace$REPLACE_KEYS_FROM_M2CE)" | tee "${SCRIPTS_DIR}/../../composer.json"
+cat "${SCRIPTS_DIR}/../../magento/magento2ee/composer.json" | jq --indent 4 "del(.replace$REPLACE_KEYS_FROM_M2CE) | del(.autoload)" | tee "${SCRIPTS_DIR}/../../magento/magento2ee/composer.json"
