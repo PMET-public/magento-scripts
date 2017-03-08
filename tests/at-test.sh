e5ee8d0a12e98976939d4dc5ea2a83f161fa2a4c
Keith Bentrup <keith@keithbentrup.com>
2017-03-08 17:54:16 -0500

    add comments and suggested crontab entry

--- tests/at-test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# stop on errors
-set -e
-# turn on debugging
-set -x
-
-echo 'hello
-world' > del-me
--- tests/cloud-cron-test.sh
+++ tests/cloud-cron-test.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
 # stop on errors
-set -e
+# set -e
 # turn on debugging
 # set -x
 
+# automate this test w/ a crontab entry like:
+# */5 * * * * sleep 30 & $0
+# 2,7,12,17,22,27,32,37,42,47,52,57 * * * * sleep 30 & $0
 
+# each of these projects is on a different host
 # xpwgonshm6qm2 rnw4cirxgmzx4 5c3fsutojnf5q 2m6nruvxa44h6 jyvegogwpv3su
 
-# echo $(date "+%Y-%m-%d %H:%M:%S") - local >> $0.log
-for i in xpwgonshm6qm2 rnw4cirxgmzx4 5c3fsutojnf5q 2m6nruvxa44h6 jyvegogwpv3su; do
+# run on these projects on different hosts (skipping 1 that seems to fail too often)
+for i in xpwgonshm6qm2 rnw4cirxgmzx4 5c3fsutojnf5q 2m6nruvxa44h6; do
+  # run several cmds but collect the output into 1 line
+  # also run the cmds in parallel
   (magento-cloud ssh -p $i -e master 'output=$(
     date "+%Y-%m-%d %H:%M:%S"
     time (php bin/magento cache:flush > /dev/null && curl -Ls localhost/women.html > /dev/null) 2>&1
     nproc
     cat /proc/loadavg | cut -d " " -f 1-3
     hostname
-  ); echo $output' >> $0.log) &
+  ); echo $output' >> dirname "$(readlink -f "$0")"/$0.log) &
 done
-
-wait
