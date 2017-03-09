3457acb13be306cd78c799394fde5796d2549a1d
Keith Bentrup <keith@keithbentrup.com>
2017-03-08 20:29:07 -0500

    use full path and change time format

--- tests/cloud-cron-test.sh
+++ tests/cloud-cron-test.sh
@@ -5,9 +5,10 @@
 # turn on debugging
 # set -x
 
+# make sure your local system time minutes and seconds match the remote
 # automate this test w/ a crontab entry like:
-# */5 * * * * sleep 30 & $0
-# 2,7,12,17,22,27,32,37,42,47,52,57 * * * * sleep 30 & $0
+# */5 * * * * sleep 30 && path_to_script
+# 2,7,12,17,22,27,32,37,42,47,52,57 * * * * sleep 30 && path_to_script
 
 # each of these projects is on a different host
 # xpwgonshm6qm2 rnw4cirxgmzx4 5c3fsutojnf5q 2m6nruvxa44h6 jyvegogwpv3su
@@ -16,11 +17,11 @@
 for i in xpwgonshm6qm2 rnw4cirxgmzx4 5c3fsutojnf5q 2m6nruvxa44h6; do
   # run several cmds but collect the output into 1 line
   # also run the cmds in parallel
-  (magento-cloud ssh -p $i -e master 'output=$(
+  (/Users/kbentrup/.magento-cloud/bin/magento-cloud ssh -p $i -e master -i /Users/kbentrup/.ssh/private-keys/id_rsa.magento 'output=$(
     date "+%Y-%m-%d %H:%M:%S"
-    time (php bin/magento cache:flush > /dev/null && curl -Ls localhost/women.html > /dev/null) 2>&1
+    time -p (php bin/magento cache:flush > /dev/null && curl -Ls localhost/women.html > /dev/null) 2>&1
     nproc
     cat /proc/loadavg | cut -d " " -f 1-3
     hostname
-  ); echo $output' >> dirname "$(readlink -f "$0")"/$0.log) &
+  ); echo $output' >> $0.log) &
 done
