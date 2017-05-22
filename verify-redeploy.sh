f174275b66f6b039bcf4b8fcbe087d43448ff872
Keith Bentrup <keith@keithbentrup.com>
2017-05-22 09:56:05 -0400

    check if deploy should continue

--- verify-redeploy.sh
+++ verify-redeploy.sh
@@ -2,15 +2,9 @@
 
 # turn on debugging
 set -x
-MAGENTO_CLOUD_TREE_ID_FILE=./var/.MAGENTO_CLOUD_TREE_ID
 
-
-if [ ! -f ${MAGENTO_CLOUD_TREE_ID_FILE} ] || [ "$(cat ${MAGENTO_CLOUD_TREE_ID_FILE})" != "${MAGENTO_CLOUD_TREE_ID}" ]; then
-  echo -n "${MAGENTO_CLOUD_TREE_ID}" > "${MAGENTO_CLOUD_TREE_ID_FILE}"
+if [ ! -f ./var/.MAGENTO_CLOUD_TREE_ID ] || [ "$(cat ./var/.MAGENTO_CLOUD_TREE_ID)" != "${MAGENTO_CLOUD_TREE_ID}" ]; then
+  echo -n "${MAGENTO_CLOUD_TREE_ID}" > "./var/.MAGENTO_CLOUD_TREE_ID"
   echo "New slug recorded: ${MAGENTO_CLOUD_TREE_ID}" >> /tmp/deploy.log
   exit 0
 fi
-
-# with a non-zero exit status, the rest of the cmds should not continue
-echo "Skipping rest of deploy ..." >> /tmp/deploy.log
-exit 1
