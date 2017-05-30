133d9794f153ad24f32baf2e94cd05f63a8d4100
Keith Bentrup <keith@keithbentrup.com>
2017-05-30 11:01:29 -0400

    print status msg in log

--- record-tree-id.sh
+++ record-tree-id.sh
@@ -7,4 +7,6 @@ if [ ! -f ./var/.MAGENTO_CLOUD_TREE_ID ] || [ "$(cat ./var/.MAGENTO_CLOUD_TREE_I
   echo -n "${MAGENTO_CLOUD_TREE_ID}" > "./var/.MAGENTO_CLOUD_TREE_ID"
   echo "New slug recorded: ${MAGENTO_CLOUD_TREE_ID}" >> /tmp/deploy.log
   exit 0
+else
+  echo "Slug unchanged." >> /tmp/deploy.log
 fi
