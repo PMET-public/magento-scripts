5e02c7801695237c24b5da5f64a8f6e074b59688
Keith Bentrup <keith@keithbentrup.com>
2017-07-19 15:08:25 -0400

    cloud ui vars must be retrieved, decoded, and parsed from MAGENTO_CLOUD_VARIABLES

--- should-redeploy.sh
+++ should-redeploy.sh
@@ -3,10 +3,14 @@
 # turn on debugging
 set -x
 
+get_env_var () {
+ echo "$MAGENTO_CLOUD_VARIABLES" | base64 --decode | python -c "import sys, json; print json.load(sys.stdin)['$1']"
+}
+
 if [ ! -f ./var/.MAGENTO_CLOUD_TREE_ID ] || \
   [ "$(cat ./var/.MAGENTO_CLOUD_TREE_ID)" != "${MAGENTO_CLOUD_TREE_ID}" ] || \
-  [ "${REDEPLOY_ENV}" = "true" ] || \
-  [ "${REDEPLOY_ENV}" = "1" ]; then
+  [ "$(get_env_var REDEPLOY_ENV)" = "true" ] || \
+  [ "$(get_env_var REDEPLOY_ENV)" = "1" ]; then
   exit 0
 else
   exit 1
