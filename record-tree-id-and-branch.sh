1c37008091e3f95491605abf270e67c3c3f94883
Keith Bentrup <keith@keithbentrup.com>
2017-08-18 12:30:15 -0400

    record branch, too

--- record-tree-id-and-branch.sh
+++ record-tree-id-and-branch.sh
@@ -10,3 +10,11 @@ if [ ! -f ./var/.MAGENTO_CLOUD_TREE_ID ] || [ "$(cat ./var/.MAGENTO_CLOUD_TREE_I
 else
   echo "Slug unchanged." >> /tmp/deploy.log
 fi
+
+if [ ! -f ./var/.MAGENTO_CLOUD_BRANCH ] || [ "$(cat ./var/.MAGENTO_CLOUD_BRANCH)" != "${MAGENTO_CLOUD_BRANCH}" ]; then
+  echo -n "${MAGENTO_CLOUD_BRANCH}" > "./var/.MAGENTO_CLOUD_BRANCH"
+  echo "New branch recorded: ${MAGENTO_CLOUD_BRANCH}" >> /tmp/deploy.log
+  exit 0
+else
+  echo "Branch unchanged." >> /tmp/deploy.log
+fi
