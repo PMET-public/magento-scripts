6376be29660b8a2ae842cf35949e393ab507cea8
Keith Bentrup <keith@keithbentrup.com>
2017-09-25 21:30:34 -0400

    changes for 2.2

--- composer-setup.sh
+++ composer-setup.sh
@@ -5,57 +5,5 @@ set -e
 # turn on debugging
 set -x
 
-function delVendorGitDirs {
-  find vendor -path '*/.git/*' -delete
-}
-
-function rsyncM2CE {
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2ce/ ./"
-}
-
-function rsyncM2EE {
-  rsyncM2CE
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2ee/ ./"
-}
-
-function rsyncM2EESampleData {
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2-sample-data/ ./"
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2-sample-data-ee/ ./"
-}
-
-function rsyncM2B2B {
-  rsyncM2EE
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2b2b/ ./"
-}
-
-function rsyncM2B2BSampleData {
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magentoese/module-b2b-media-sample-data/ ./pub/media/"
-}
-
-function createMediaDirs {
-  mkdir -p ./pub/media/catalog/product ./pub/media/downloadable/spec_sheets ./pub/media/wysiwyg/home
-}
-
-rsyncOpts="-rlptz --exclude '/composer.*' --exclude '/.git*' --exclude '/README.md' --exclude '/LICENSE*'"
-isPlatform=$(test -e /etc/platform/boot && echo "true" || echo "")
-[ $isPlatform ] && rsyncOpts="$rsyncOpts --remove-source-files"
-[ $isPlatform ] && delVendorGitDirs
-
-case $1 in
-  ce)
-    rsyncM2CE
-  ;;
-  ref)
-    rsyncM2EE; rsyncM2EESampleData || echo ""
-  ;;
-  demo)
-    rsyncM2EE; rsyncM2EESampleData || echo ""
-    /bin/bash -c "rsync $rsyncOpts ./vendor/magentoese/module-venia-media-sample-data/ ./pub/media/" || echo ""
-  ;;
-  b2b)
-    rsyncM2B2B; createMediaDirs; rsyncM2B2BSampleData
-  ;;
-esac
-
 # enable error reporting
 mv ./pub/errors/local.xml.sample ./pub/errors/local.xml || :
