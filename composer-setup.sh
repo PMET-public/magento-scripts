11cdf19a7253bf82fffb2f19a8385ccb1494cea1
Keith Bentrup <keith@keithbentrup.com>
2017-10-26 09:20:07 -0400

    prevent error when file already deleted

--- composer-setup.sh
+++ composer-setup.sh
@@ -59,7 +59,7 @@ case $1 in
 esac
 
 # remove .htaccess file b/c not web root
-rm .htaccess
+rm .htaccess || :
 
 # enable error reporting
 mv ./pub/errors/local.xml.sample ./pub/errors/local.xml || :
