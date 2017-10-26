77bee03ad62b3f3e69a1c157c504c844b5a4d77c
Keith Bentrup <keith@keithbentrup.com>
2017-10-26 18:41:14 -0400

    add deploy script for additional steps

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# stop on errors
+set -e
+# turn on debugging
+set -x
+
+INITIALIZED_FLAG_FILE=/app/init/app/etc/.initialized
+if [ ! -f "${INITIALIZED_FLAG_FILE}" ]; then
+
+  /app/bin/magento module:enable MagentoEse_PostInstall
+  /app/bin/magento setup:upgrade --keep-generated -n 
+
+  touch "${INITIALIZED_FLAG_FILE}"
+fi
