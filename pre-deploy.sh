7c6dfeca5a2f702cffbd94ad2355a0dedd92272b
Keith Bentrup <keith@keithbentrup.com>
2017-10-31 23:41:21 -0400

    database will already exist

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -12,6 +12,4 @@ if [ ! -f "${INITIALIZED_FLAG_FILE}" ]; then
   /app/bin/magento module:enable --all
   /app/bin/magento module:disable MagentoEse_PostInstall
 
-  mysql -C --host=database.internal -NBe "DROP DATABASE IF EXISTS \`main\`; CREATE DATABASE IF NOT EXISTS \`main\` DEFAULT CHARACTER SET utf8;"
-
 fi
