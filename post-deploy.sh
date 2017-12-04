a3135874e3d0431f6e560770c6e08194d3761473
Keith Bentrup <keith@keithbentrup.com>
2017-12-04 12:28:22 -0500

    update conditional syntax

--- post-deploy.sh
+++ post-deploy.sh
@@ -7,16 +7,19 @@ set -x
 
 INITIALIZED_FLAG_FILE=/app/init/app/etc/.initialized
 
+is_ref=$(grep -q '"magento/module-catalog-sample-data"' /app/composer.json && ! grep -q '"magentoese/module-demo-admin-configurations"' /app/composer.json && echo "true" || echo "false")
+
+
 if [ ! -f "${INITIALIZED_FLAG_FILE}" ]; then
 
   # we for an  initial indexing in demo, but ref does not.
   # so index initially if composer appears to be ref
-  if grep -q '"magento/module-catalog-sample-data"' /app/composer.json && ! grep -q '"magentoese/module-demo-admin-configurations"' /app/composer.json; then
+  if [ $is_ref == "true" ]; then
     /app/bin/magento indexer:reindex
+  else
+    /app/bin/magento module:enable MagentoEse_PostInstall
+    /app/bin/magento setup:upgrade --keep-generated -n
   fi
-
-  /app/bin/magento module:enable MagentoEse_PostInstall
-  /app/bin/magento setup:upgrade --keep-generated -n 
 
   touch "${INITIALIZED_FLAG_FILE}"
 fi
