5069df53ca4ce47eea8bd6d49ca42a53ea11015c
Keith Bentrup <keith@keithbentrup.com>
2017-12-04 09:38:01 -0500

    add reindex for ref cmd

--- post-deploy.sh
+++ post-deploy.sh
@@ -9,8 +9,15 @@ INITIALIZED_FLAG_FILE=/app/init/app/etc/.initialized
 
 if [ ! -f "${INITIALIZED_FLAG_FILE}" ]; then
 
+  # we for an  initial indexing in demo, but ref does not.
+  # so index initially if composer appears to be ref
+  if [ grep -q '"magento/module-catalog-sample-data"' /app/composer.json  -a ! grep -q '"magentoese/module-demo-admin-configurations"' /app/composer.json ]; then
+    /app/bin/magento indexer:reindex
+  fi
+
   /app/bin/magento module:enable MagentoEse_PostInstall
   /app/bin/magento setup:upgrade --keep-generated -n 
 
   touch "${INITIALIZED_FLAG_FILE}"
 fi
+
