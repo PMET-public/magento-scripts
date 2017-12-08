15b74db44402473968bf8b1bc6a5965ee0a895a3
Keith Bentrup <keith@keithbentrup.com>
2017-12-08 11:20:09 -0500

    edits for consistency and optimization of deployment

--- composer-setup.sh
+++ composer-setup.sh
@@ -5,58 +5,28 @@ set -e
 # turn on debugging
 set -x
 
-function delVendorGitDirs {
-  find vendor -path '*/.git/*' -delete
-}
-
-function rsyncM2CE {
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2ce/ ./"
-}
-
-function rsyncM2EE {
-  rsyncM2CE
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2ee/ ./"
-}
-
-function rsyncM2EESampleData {
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2-sample-data/ ./"
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2-sample-data-ee/ ./"
-}
-
-function rsyncM2B2B {
-  rsyncM2EE
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magento/magento2b2b/ ./"
-}
-
-function rsyncM2B2BSampleData {
-  /bin/bash -c "rsync $rsyncOpts ./vendor/magentoese/module-b2b-media-sample-data/ ./pub/media/"
-}
-
-function createMediaDirs {
-  mkdir -p ./pub/media/catalog/product ./pub/media/downloadable/spec_sheets ./pub/media/wysiwyg/home
-}
-
-rsyncOpts="-rlptz --exclude '/composer.*' --exclude '/.git*' --exclude '/README.md' --exclude '/LICENSE*'"
-isPlatformEnv=$(test -e /etc/platform/boot && echo "true" || echo "")
-[ $isPlatformEnv ] && rsyncOpts="$rsyncOpts --remove-source-files"
-[ $isPlatformEnv ] && delVendorGitDirs
+SCRIPTS_DIR=$( cd $(dirname $0) ; pwd -P )
+. "${SCRIPTS_DIR}/lib.sh"
 
 case $1 in
   ce)
-    # rsyncM2CE
   ;;
   ref)
-    # rsyncM2EE; rsyncM2EESampleData || echo ""
   ;;
   demo)
-    # rsyncM2EE; rsyncM2EESampleData || echo ""
-    /bin/bash -c "rsync $rsyncOpts ./vendor/magentoese/module-venia-media-sample-data/ ./pub/media/" || echo ""
+    /bin/bash -c "rsync $RSYNC_OPTS ./vendor/magentoese/module-venia-media-sample-data/ ./pub/media/" || echo ""
   ;;
   b2b)
-    # rsyncM2B2B; 
-    createMediaDirs; rsyncM2B2BSampleData
+    mkdir -p ./pub/media/catalog/product ./pub/media/downloadable/spec_sheets ./pub/media/wysiwyg/home
+    /bin/bash -c "rsync $RSYNC_OPTS ./vendor/magentoese/module-b2b-media-sample-data/ ./pub/media/"
   ;;
 esac
+
+# remove files not needed for deployment on platform
+if [ "${IS_PLATFORM_ENV}" == "true" ]; then
+  find vendor -type d \( -path "*/dev" -o -path "*/Test" -o -path "*/.git" \) -exec rm -rf {} \;
+  rm -rf init/pub/media/* vendor/magento/sample-data-media
+fi
 
 # enable error reporting
 mv ./pub/errors/local.xml.sample ./pub/errors/local.xml || :
--- lib.sh
+++ lib.sh
@@ -9,4 +9,8 @@ set -a
 
 INITIALIZED_FLAG_FILE=/app/init/app/etc/.initialized
 
-is_ref=$(grep -q '"magento/module-catalog-sample-data"' /app/composer.json && ! grep -q '"magentoese/module-demo-admin-configurations"' /app/composer.json && echo "true" || echo "false")
+IS_REF=$(grep -q '"magento/module-catalog-sample-data"' /app/composer.json && ! grep -q '"magentoese/module-demo-admin-configurations"' /app/composer.json && echo "true" || echo "false")
+
+IS_PLATFORM_ENV=$(test -e /etc/platform/boot && echo "true" || echo "false")
+RSYNC_OPTS="-rlptz --exclude '/composer.*' --exclude '/.git*' --exclude '/README.md' --exclude '/LICENSE*'"
+[ "${IS_PLATFORM_ENV}" == "true" ] && RSYNC_OPTS="$RSYNC_OPTS --remove-source-files"
