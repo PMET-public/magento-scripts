2f2775a1aa83c08d6e920212bb0459f031ca6244
Keith Bentrup <keith@keithbentrup.com>
2019-03-11 01:38:39 -0400

    remove vendor media files sync to pub/media since magento no longer imports in place

--- composer-setup.sh
+++ composer-setup.sh
@@ -6,25 +6,7 @@ set -x
 cur_dir=$( cd $(dirname $0) ; pwd -P )
 . $cur_dir/lib.sh
 
-case $1 in
-  ce)
-  ;;
-  ref)
-    rsyncSampleMedia
-  ;;
-  demo)
-    rsyncSampleMedia
-    /bin/bash -c "rsync $COMPOSER_RSYNC_OPTS vendor/magentoese/module-venia-media-sample-data/ pub/media/ || :"
-    /bin/bash -c "rsync $COMPOSER_RSYNC_OPTS vendor/magentoese/module-cms-media-sample-data-update/ pub/media/ || :"
-  ;;
-  b2b)
-    mkdir -p ./pub/media/catalog/product ./pub/media/downloadable/spec_sheets ./pub/media/wysiwyg/home
-    /bin/bash -c "rsync $COMPOSER_RSYNC_OPTS vendor/magentoese/module-b2b-media-sample-data/ pub/media/ || :"
-  ;;
-esac
-
 # remove files not needed for deployment on platform
 if is_platform_env; then
   find vendor -type d \( -path "*/.git" \) -exec rm -rf {} \; 2>/dev/null || :
-  rm -rf vendor/magento/sample-data-media || :
 fi
